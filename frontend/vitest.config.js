f5f9991aca849652ca69fd03f5a0956ff0202f04
Naveen Badiger <83811961+Naveenub@users.noreply.github.com>
2026-05-24 17:41:52 +0530

    Update vitest.config.js

--- frontend/vitest.config.js
+++ frontend/vitest.config.js
@@ -2,7 +2,8 @@ import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react({ include: /\.(jsx?|tsx?)$/ })],
+  esbuild: { jsx: 'automatic' },
   test: {
     globals: true,
     environment: "jsdom",
